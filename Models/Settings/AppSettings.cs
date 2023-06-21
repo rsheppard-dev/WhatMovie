c9ceec366d83353b644d6d0c0a48a5288e34469e
Roy Sheppard <rsheppard83@gmail.com>
2023-06-21 22:14:51 +0100

    added featured movie categories to homepage in carousels

--- Models/Settings/AppSettings.cs
+++ Models/Settings/AppSettings.cs
@@ -4,5 +4,20 @@ namespace WhatMovie.Models.Settings
     {
         public WhatMovieSettings? WhatMovieSettings { get; set; }
         public TMDBSettings? TMDBSettings { get; set; }
+        public ApiSettings? ApiSettings { get; set; }
+        public DefaultCredentials? DefaultCredentials { get; set; }
+    }
+
+    public class ApiSettings
+    {
+        public string? TMDBApiKey { get; set; }
+        public string? TMDBReadAccessToken { get; set; }
+    }
+    public class DefaultCredentials
+    {
+        public string? Role { get; set;}
+        public string? Email { get; set; }
+        public string? Password { get; set;}
+
     }
 }
